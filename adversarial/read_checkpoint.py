f27451c100da0ee3dea4811e65932808961d8da6
Connor-XY <xu.yan@pku.edu.cn>
2021-12-19 04:53:59 +0000

    read_checkpoint

--- adversarial/read_checkpoint.py
+++ adversarial/read_checkpoint.py
@@ -1,5 +1,6 @@
 from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
-# print_tensors_in_checkpoint_file(file_name='gs://grover-models/discrimination/generator=base~discriminator=grover~discsize=base~dataset=p=0.96/model.ckpt-1562')
+import numpy as np
+#print_tensors_in_checkpoint_file(file_name='gs://grover-models/discrimination/generator=base~discriminator=grover~discsize=base~dataset=p=0.96/model.ckpt-1562',tensor_name='newslm/embeddings/word_embed',all_tensors=False)
 
 from tensorflow.python import pywrap_tensorflow
 import os
@@ -9,5 +10,9 @@ reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)
 var_to_shape_map = reader.get_variable_to_shape_map()
 
 for key in var_to_shape_map:
-    print("tensor_name: ", key)
+    if key == 'newslm/embeddings/word_embed':
+        with open('embedding.npy', 'wb') as f:
+            np.save(f, reader.get_tensor(key))
+    #pass
+    #print("tensor_name: ", key)
     # print(reader.get_tensor(key)) # Remove this is you want to print only variable names
